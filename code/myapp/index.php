c85fa7ba64a70e4ce135a4411ea12f4c64a12603
makros <lorente.marcos@gmail.com>
2017-12-04 18:53:10 +0100

    Modificación en index.php

--- code/myapp/index.php
+++ code/myapp/index.php
@@ -1,1 +1,1 @@
-<?= 'Se ha lanzado la aplicación correctamente. Basado en PHP 7.2 + NGinx + MySQL'; ?>
+<?= <h1>'Se ha lanzado la aplicación correctamente. Basado en PHP 7.2 + NGinx + MySQL' </h1> ?>
